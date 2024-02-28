06aaf839e3a1b1beff4fed7284c078620436ba7d
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-02-28 23:17:47 +0500

    Step02-Variables added

---
+++ Step03-Variables/app.js
@@ -0,0 +1,37 @@
+// Variable :-
+    //  "The word Variable means 'thing which can vary/change', all programming languages uses variables to store some value which can be reused after ."
+
+    // Similarly,
+        //In JavaSript, variable is like a container in which some kind of value/Datatype. 
+        //  We declare variables to store some value (i.e string,number & boolean etc)   
+
+// Ways of Declaring Variables in JavaScript :-
+            // In JavaScript, there are three keywords to declare variables such as
+            
+            // 1- var 
+            // 2- let 
+            // 3- const.
+            
+// Var :-
+    // "Var" is a keyword for declaring variables in JavaScript . In old JavaScript , we use "var" keyword for declaration of variable but due to some flaws JavaScript introduces "let".
+    
+// Syntax of Declaring Variable 
+var myname="Shahzaib";
+
+// let :-
+      // "let" is a keyword for declaring variables in JavaScript .
+
+// Syntax of Declaring Variable 
+let yourname="Alex";
+
+// const :-
+  // "const" is a keyword for declaring variables in JavaScript .
+
+const gravity=9.8;
+
+
+// We can also modify value after declaration and assignment  of variable by re-assigning it to the new value.
+// Such as 
+
+var city ="Karachi";//Declaration and Assignment of Variable
+city ="lahore";//Reassigning Variable
