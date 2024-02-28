2cc860a1629e6b21982944add4d7e6f3da749722
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-02-28 23:47:17 +0500

    Step03-Datatypes added

---
+++ Step03-Datatypes/app.js
@@ -0,0 +1,45 @@
+// Datatypes :-
+    //  "Datatypes" means 'the type of Data' stored in vriable , array etc 
+
+    // There are two types of Datatypes in JavaScript;
+
+    // 1-Primitive Datatypes :-
+            //  "Primitive Datatypes"  are those which are stored in memory by value.
+            
+            // Primitive Datatypes are subdivided into five Datatypes;
+
+            // a-String 
+            // b-Number
+            // c-Boolean
+            // d-Null
+            // e-undefined
+
+
+            // a-String:-
+                //    String is a primitive Datatype  which can store value in the form of text . It is enclosed inside double quotation_marks("");
+                
+                var my_name = "Shahzaib";
+
+                
+
+            // b-Number:-
+                //    Number is a primitive Datatype  which can store value in the form of number .
+                
+                var age = 16;
+
+                // c-Boolean :-
+                    //   Boolean is a primitive Datatype which has only two reserve value (either true or false)
+                    // It is mostl used in coditional Operations 
+
+                    var foo1 = true;//Boolean
+                    var foo2= true;//Boolean
+
+                // d-Null:-
+                     //   Null is a primitive Datatype . The value of Null is not assigned at the time of declaration of Variable that's why it is called as "Null Datatype"
+
+                     var student="";
+
+                // d-undefined:-
+                     //   undefined is a primitive Datatype . The value of Undefined is not assigned at the time of declaration of Variable but it is declare somewhere after in the project that's why it is called as "Null Datatype" 
+
+                     var student2;//undefined
