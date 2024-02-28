57947a4b236468fd68ed0bc7bd34fe4631120bb5
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-02-28 22:42:00 +0500

    Step01-Alerts Added

---
+++ Step01-Alerts/app.js
@@ -0,0 +1,7 @@
+// Alerts:-
+    //  "In JavaScript,Alert is a built-in function which is used to show pop-up notifications to the User during the Loading of Webpage"
+    
+    // The Syntax of Alert is ;
+    alert("Hello World! I am entering in the world of Coding");
+
+    //Note:- Alert always runs on UI ;
