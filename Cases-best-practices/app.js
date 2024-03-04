29efe02575fb7dd1aeb4a26919796bc925ee6a7b
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-04 23:26:16 +0500

    Cases are Added

---
+++ Cases-best-practices/app.js
@@ -0,0 +1,30 @@
+// Cases in JavaScript :- 
+
+// There are many ways in JavaScript for declaring name of Variables , Function etc . But the readability of code is not too good so far .
+
+// Therefore , there are some Cases which are effective and best to use when declaring names of Variables and Functions.
+// These cases are introduced to enhance readability of Long Chained Variable Names .
+
+// 1-Camel Case
+// 2-Snake Case
+
+// 1- Camel Case:-
+            // Camel Case Idea was introduced by some developers for enhencing the readability of Code .
+            // This is one of the best practices for declaring Variables and Functions names.
+            // In Camel , the first Letter of every new word in the sequence will be capital and the other letters will be small.
+            
+            // For Example :- 
+             var guyWhoKeepRepeatingHisName = "Shahzaib";
+             // So the above phrase is a bit easy ti read than;
+             var guywhokeeprepeatinghisname = "Shahzaib";
+             
+    
+ //  2-Snake Case :- 
+             // Snake Case Idea is the most popular and best case to use for declaring Variables and Functions names.
+             // In Snake Case , an UnderScore ( _ ) adds after the end of every word in the sequence.         
+             
+             // For Example :- 
+             var guy_who_keep_repeating_his_name = "Shahzaib";
+
+            //  So this is very to read and understand 
+            
