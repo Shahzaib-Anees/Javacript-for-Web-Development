bf23a2f177924c926d04321873421f7a0c0b23f1
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-04 23:02:24 +0500

    Variable name declaration rules added

---
+++ Step02-Variables/Step02a-Rules for Naming Variables/app.js
@@ -0,0 +1,17 @@
+// Rules for Naming Variables :- 
+     
+// As we discuss about the variables previously,
+
+// There are few rules which are necessay to follow when we declare Variable Names in JavaScript.
+
+// 1- Variable names cannot contains whitespaces/spaces .
+// var my name = "Shahzaib";     //Error
+// 2- Variable names cannot starts with a number.
+// var 2rabbits_color = "Black"  //Error
+
+// 3- Variable names do not allow any Special Character except dollar ("$") and underscore ("_");
+var $fruits = "Oranges";
+var my_name = "Shahzaib";
+
+// 4-Variable names also not allows hifen (-) to use when declaring.
+// var car-name = "Ferrari"      //Error
