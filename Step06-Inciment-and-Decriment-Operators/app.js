07b5a1cb161e643f2ad351ef270db76ebd3d2da7
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-08 21:40:00 +0500

    Incriment Operators added

---
+++ Step06-Inciment-and-Decriment-Operators/app.js
@@ -0,0 +1,31 @@
+// Incriment and Decriment Operators :- 
+
+// JavaScript uses some Unfamiliar for Adding and Substracting numbers from the present state which are known as "Incriment and Decriment Operators" .
+// These Operators have a real significance in realtime Programming.
+
+// Incriment Operators :- 
+    //  The word Incriment means "To Incease or Add" .Similarly, In JavaScript Incrimnet is used to add a single(1) value in the existing number(variable);
+
+    var num = 3
+    // If I want to add 1 in above variable ,I can simply use Incriment operator.
+
+    num++ //Incriment Operator
+
+    // Types of Incriment :-
+
+    // There are two Incriments done by using Incriment Operator
+    1- Pre-Incriment 
+    2- Post-Incriment
+
+    // 1-Pre-Incriment :- 
+
+    // Pre incriment adds the value on the same line.
+    
+    ++num // Pre-Incriment 
+    
+    // 2- Post-Incriment:-
+    
+    // Pre incriment adds the value on the next line.
+    num ++ //Post-Incriment
+
+    
