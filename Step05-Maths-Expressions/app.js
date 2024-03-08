0815ccf4696ce8c21f6d9a914ba5dc020c8aa66f
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-08 21:23:23 +0500

    Info about Maths Expressions Added

---
+++ Step05-Maths-Expressions/app.js
@@ -0,0 +1,29 @@
+// Maths Expressions/Operations in JavaScript :-
+
+// In JavaScript, we can also perform mathematical operations like Addition,Multiplications , Division and Substractions 
+
+var num1= 2;
+var num2 = 5;
+
+// For Addtion/Sum of numbers :-
+
+console.log(num1 + num2);
+
+// For SUbstractions of numbers :-
+console.log(num1 - num2);
+
+// For Multiplication of numbers :-
+console.log(num1 * num2);
+
+
+// For Division of numbers :-
+console.log(num1 / num2);
+
+
+// JavaScript follows "BODMAS" priority order therefore, If we want to remove Ambiguity in any Airthmetic we can add Brackets.
+
+// For Example 
+
+// If I want to multiply above two variables first and then divides the result by 2 then I can add brackets in the multiplication of two numbers to increase theor Priority Order.
+console.log(num1 * num2/2)//Wrong Result (Ambigious)
+console.log((num1 * num2)/2)//True Case
