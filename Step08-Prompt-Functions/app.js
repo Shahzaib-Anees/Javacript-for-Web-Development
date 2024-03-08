1f55f2892e8edf73e55663bbec014841ba79636c
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-08 22:02:23 +0500

    Prompt Function Added

--- Step08-Prompt-Functions/app.js
+++ Step08-Prompt-Functions/app.js
@@ -3,4 +3,5 @@
 // Prompt in JavaScript is used to take the input from the user to the server .
 
 
-var user_input = prompt("Enetr your name");
+var user_input = prompt("Enetr your name");//For Input
+console.log(user_input);
