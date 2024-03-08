536e39a9e4c492062d12dbe782d9162b070efc8c
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-08 21:44:31 +0500

    Decriment Operator Added

--- Step06-Inciment-and-Decriment-Operators/app.js
+++ Step06-Inciment-and-Decriment-Operators/app.js
@@ -28,4 +28,26 @@
     // Pre incriment adds the value on the next line.
     num ++ //Post-Incriment
 
+// Decriment Operators :- 
+    //  The word Decriment means "To Decease or Substract" .Similarly, In JavaScript Decriment Operator is used to substract a single(1) value in the existing number(variable);
+    var num1 = 5
+    // If I want to substract 1 in above variable ,I can simply use Decriment operator.
+    num1-- //Decriment Operator
+
+
+    // Types of Decriment :-
+
+    // There are two Decriments done by using Incriment Operator
+    1- Pre-Decriment 
+    2- Post-Decriment
+
+    // 1-Pre-Decriment :- 
+
+    // Pre Decriment substracts the value on the same line.
     
+    --num1 // Pre-Decriment 
+    
+    // 2- Post-Decriment:-
+    
+    // Pre Decriment substracts the value on the next line.
+    num1 -- //Post-Decriment
